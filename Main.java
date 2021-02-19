1fc01974605a25bfb2a4a37439e9c19e8ab840e6
tsvetanvashkov <tsvetan.vashkov@gmail.com>
2021-02-19 23:50:51 +0200

    Java Advanced

---
+++ Main.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		CustomStack customStack =new CustomStack();
+		String[] initialValues = scan.nextLine().split(",\\s+");
+		
+		customStack.push(Integer.parseInt(initialValues[0].split("\\s")[1]));
+		
+		for (int i = 1; i < initialValues.length; i++) {
+			customStack.push(Integer.parseInt(initialValues[i].trim()));
+		}
+		
+		String line = scan.nextLine();
+		while (!line.equals("END")) {
+			if (line.equals("Pop")) {
+				try {
+					customStack.pop();
+				} catch (IllegalStateException e) {
+					System.out.println(e.getMessage());
+				}
+			}else {
+				customStack.push(Integer.parseInt(line.split("\\s+")[1]));
+			}
+			line = scan.nextLine();
+		}
+		scan.close();
+		for (int i = 0; i < 2; i++) {
+			for (Integer element : customStack) {
+				System.out.println(element);
+			}
+		}
+		
+	}
+
+}
